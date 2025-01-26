63d47ea77ff4310b0daa362e3e21a0917554fe6c
Mark <mark.kietnghiem@gmail.com>
2025-01-25 22:48:06 -0800

    feat: Adding about-me

---
+++ src/components/About-AboutMe.jsx
@@ -0,0 +1,20 @@
+const AboutMe = () => {
+  return (
+    <div id="about-me">
+      <h3>About Me...</h3>
+      <p>
+        I am a dedicated and hungry-for-knowledge software engineer, always
+        striving to improve and enrich myself with self-education and hands-on
+        experiences in building applications and technologies with the goal of
+        improving both UX and DX while adhering to best practices and staying
+        informed with the latest technologies.
+      </p>
+      <p>
+        My expertise is in building web applications and frameworks with
+        JavaScript, TypeScript, React.js and Node.js.
+      </p>
+    </div>
+  );
+};
+
+export default AboutMe;
