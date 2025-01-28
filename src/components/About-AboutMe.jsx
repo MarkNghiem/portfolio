68598e2c664ed33048f72e87f75b4630551cae06
Mark <mark.kietnghiem@gmail.com>
2025-01-27 19:45:21 -0800

    style: Rough draft for AboutMe

--- src/components/About-AboutMe.jsx
+++ src/components/About-AboutMe.jsx
@@ -1,3 +1,6 @@
+import chuchuLarge from '../../public/chuchu-large.jpeg';
+import pikaLarge from '../../public/pika-large.jpeg';
+
 const AboutMe = () => {
   return (
     <div id="about-me">
@@ -13,6 +16,13 @@ const AboutMe = () => {
         My expertise is in building web applications and frameworks with
         JavaScript, TypeScript, React.js and Node.js.
       </p>
+      <h3>Meet My Partners!</h3>
+      <img src={pikaLarge} title='Pika' aria-label='Pika' className='size-60 object-cover rounded-4xl' />
+      <h4>Pika</h4>
+      <p>My first ever kitty I adopted since 2016. He was 6 months old when I first met him at a local shelter. Since then he has been very needy and always want snuggle times but very shy when it comes to stranger</p>
+      <img src={chuchuLarge} title='Chuchu' aria-label='Chuchu' className=' size-60 object-cover rounded-4xl'/>
+      <h4>Chuchu</h4>
+      <p>Pika&apos;s little brother. Came home a few months after Pika. He&apos;s a bit more independent compared to his brother but can&apos;t resist a good belly rub.</p>
     </div>
   );
 };
