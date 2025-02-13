1867bcb17f5b5b2e86f9587ef50eb6bbbe686d07
Mark <mark.kietnghiem@gmail.com>
2025-02-13 01:23:59 -0800

    fix: Fix fade in timing for Recommendations Section

--- src/containers/EndingContainer.jsx
+++ src/containers/EndingContainer.jsx
@@ -1,18 +1,39 @@
 // A Surprised Section for reaching the end of the page
 
+import { useState, useEffect } from 'react';
+
 import PikaEnding from "../../public/pika-ending-small.jpg";
 import ChuchuEnding from "../../public/chuchu-ending-small.jpg";
 import Together from "../../public/together-ending-small.jpg";
 
 const EndingContainer = () => {
+  const [divVisible, setDivVisible] = useState(false);
+  const [contentVisible, setContentVisible] = useState(false);
+
+  // Timer to create smooth fade in effect for each components
+  useEffect(() => {
+    const divTimer = setTimeout(() => {
+      setDivVisible(true);
+    }, 6100);
+
+    const contentTimer = setTimeout(() => {
+      setContentVisible(true);
+    }, 6500);
+
+    return () => {
+      clearTimeout(divTimer);
+      clearTimeout(contentTimer);
+    };
+  });
+
   const endingPictures = [PikaEnding, Together, ChuchuEnding];
 
   return (
     <div
       id="ending"
-      className="mt-[100vh] w-4/5 place-self-center rounded-2xl bg-linear-to-br from-slate-900/20 to-amber-900/30 py-8 shadow-2xl shadow-slate-900/50 mb-20"
+      className={`mt-[100vh] mb-20 w-4/5 place-self-center rounded-2xl bg-linear-to-br from-slate-900/20 to-amber-900/30 py-8 shadow-2xl shadow-slate-900/50 transition duration-1000 ease-out ${divVisible ? 'translate-y-0 opacity-100' : '-translate-y-10 opacity-0'}`}
     >
-      <div id="ending-content" className="">
+      <div id="ending-content" className={`transition-opacity duration-1000 ease-out ${contentVisible ? 'opacity-100' : 'opacity-0'}`}>
         <h1 className="pb-5 text-5xl font-semibold font-stretch-120%">
           You have reached the end!
         </h1>
---
+++ src/containers/Footer.jsx
@@ -0,0 +1,8 @@
+/**
+ * Footer - Contains:
+ * - Another portrait of me
+ * - Ways to contact me
+ * - Techs that are used to create this website
+ * - Changelog: This website will be updated regularly
+ */
+
