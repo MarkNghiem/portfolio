db39a485db0a7b7dc111efd82c107f4cd00421a7
Mark <mark.kietnghiem@gmail.com>
2025-02-24 21:40:09 -0800

    refactor: Finished refactoring About-AboutMe

--- src/components/About-AboutMe.jsx
+++ src/components/About-AboutMe.jsx
@@ -2,15 +2,6 @@
 
 import { useEffect, useState } from "react";
 
-import {
-  about3xl1,
-  about3xl2,
-  about3xl3,
-  about3xl4,
-  flex3xl,
-  ender3xl,
-} from "../assets/styles";
-
 import chuchuLarge from "../../public/chuchu-large.jpeg";
 import pikaLarge from "../../public/pika-large.jpeg";
 
@@ -37,17 +28,13 @@ const AboutMe = () => {
   return (
     <div
       id="about-me"
-      className={`div-background div-effect text-start ${divVisible ? "translate-y-0 opacity-100" : "-translate-y-10 opacity-0"} ${about3xl1}`}
+      className={`div-background div-effect about-about-desktop-layout text-start ${divVisible ? "translate-y-0 opacity-100" : "-translate-y-10 opacity-0"}`}
     >
       <div
         id="about-me-content"
         className={`content-effect ${contentVisible ? "opacity-100" : "opacity-0"}`}
       >
-        <h2
-          className={`mb-5 text-3xl font-semibold font-stretch-115% ${ender3xl}`}
-        >
-          About Me...
-        </h2>
+        <h2 className="title mb-5">About Me...</h2>
         <p className="mb-3">
           👋🏼 Hey there! my name is Kiet Nghiem but I go by Mark most of the
           time. I am a passionate and hungry-for-knowledge Software Engineer
@@ -77,23 +64,16 @@ const AboutMe = () => {
           </span>
         </p>
         <p>Let&apos;s build something amazing together! 🚀</p>
-        <h2
-          className={`my-10 text-3xl font-semibold font-stretch-115% ${about3xl2}`}
-        >
-          Meet My Partners!
-        </h2>
-        <div
-          id="pika"
-          className={`mb-8 items-center gap-4 max-md:flex max-md:flex-col ${flex3xl}`}
-        >
+        <h2 className="subtitle my-10">Meet My Partners!</h2>
+        <div id="pika" className="col-flex partner-container mb-10">
           <img
             src={pikaLarge}
             title="Pika"
             aria-label="Pika"
-            className={`mb-5 size-60 place-self-center rounded-4xl border-2 border-slate-400 object-cover shadow-xl shadow-slate-900/40 transition-transform duration-200 hover:scale-105`}
+            className="partner-image content-border avatar-rounded-4xl content-hover-effect"
           />
           <div id="pika-details">
-            <h4 className="mb-3 text-center text-3xl font-semibold">Pika</h4>
+            <h4 className="pika-bio subtitle">Pika</h4>
             <p>
               My first ever kitty I adopted since 2016. He was 6 months old when
               I first met him at a local shelter. Since then he has been very
@@ -102,19 +82,16 @@ const AboutMe = () => {
             </p>
           </div>
         </div>
-        <div
-          id="chuchu"
-          className={`items-center gap-4 max-md:flex max-md:flex-col ${flex3xl}`}
-        >
+        <div id="chuchu" className="col-flex partner-container">
           <img
             src={chuchuLarge}
             title="Chuchu"
             aria-label="Chuchu"
-            className={`mb-5 size-60 place-self-center rounded-4xl border-2 border-slate-400 object-cover shadow-xl shadow-slate-900/40 transition-transform duration-200 hover:scale-105 ${about3xl3}`}
+            className="partner-image content-border avatar-rounded-4xl content-hover-effect lg:order-2"
           />
-          <div id="chuchu-details" className={`${about3xl4}`}>
-            <h4 className="mb-3 text-center text-3xl font-semibold">Chuchu</h4>
-            <p className="text-start">
+          <div id="chuchu-details" className="order-1">
+            <h4 className="chuchu-bio subtitle">Chuchu</h4>
+            <p className="lg:text-end">
               Pika&apos;s little brother. Came home a few months after Pika.
               He&apos;s a lot more independent compared to his brother but
               can&apos;t resist a good belly rub. He&apos;s so loveable that
